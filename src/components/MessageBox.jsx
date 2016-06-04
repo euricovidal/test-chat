f3e15c2dd2d21eb09150b3406128b22c90170195
Eurico Vidal <euricovidal@gmail.com>
2016-06-04 05:07:17 -0300

    finished cap Using Firebase

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import { Card } from 'material-ui'
+import trim from 'trim'
+import Firebase from 'firebase'
 
 class MessageBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: ''
+    }
+    this.firebase = Firebase.initializeApp({
+      apiKey:        "AIzaSyD6uKj1UY3dv8gOabzv9ITHC_4Dmj_OiG4",
+      authDomain:    "chat-4f2ed.firebaseapp.com",
+      databaseURL:   "https://chat-4f2ed.firebaseio.com",
+      storageBucket: "chat-4f2ed.appspot.com"
+    }, 'MessageBox')
+    this.database = this.firebase.database()
+  }
+  onChange(event) {
+    this.setState({
+      message: event.target.value
+    })
+  }
+  onKeyUp(event) {
+    if(event.keyCode === 13 && trim(event.target.value) !== '') {
+      event.preventDefault()
+      this.database.ref('messages').push({
+        message: this.state.message
+      })
+      this.setState({
+        message: ''
+      })
+    }
+  }
   render() {
     var card_style = {
       maxWidth: 1280,
@@ -21,7 +52,11 @@ class MessageBox extends React.Component {
 
     return(
       <Card style={ card_style }>
-        <textarea style={ textarea_style } />
+        <textarea
+          value={ this.state.message }
+          onChange={ this.onChange.bind(this) }
+          onKeyUp={ this.onKeyUp.bind(this) }
+          style={ textarea_style } />
       </Card>
     )
   }
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,21 +1,40 @@
 import React   from 'react'
 import Message from './Message.jsx'
 import { Card, List } from 'material-ui'
+import Firebase from 'firebase'
+import _ from 'lodash'
 
 class MessageList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      messages: [
-        'Hullo there how are you?',
-        'I am fine, and you?'
-      ]
+      messages: {}
     }
+
+    this.firebase = Firebase.initializeApp({
+      apiKey:        "AIzaSyD6uKj1UY3dv8gOabzv9ITHC_4Dmj_OiG4",
+      authDomain:    "chat-4f2ed.firebaseapp.com",
+      databaseURL:   "https://chat-4f2ed.firebaseio.com",
+      storageBucket: "chat-4f2ed.appspot.com"
+    }, 'MessageList')
+    this.database = this.firebase.database().ref('messages')
+    this.database.on('child_added', (msg) => {
+      if(this.state.messages[msg.key]) return
+      let msgVal = msg.val()
+      msgVal.key = msg.key
+      this.state.messages[msgVal.key] = msgVal
+      this.setState({messages: this.state.messages})
+    })
+
+    this.database.on('child_removed', (msg) => {
+      delete this.state.messages[msg.key]
+      this.setState({messages: this.state.messages})
+    })
   }
   render() {
-    var messageNodes = this.state.messages.map((message) => {
+    var messageNodes = _.values(this.state.messages).map((message) => {
       return (
-        <Message key={message} message={message} />
+        <Message key={message.message} message={message.message} />
       )
     })
     var card_style = {
